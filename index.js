98713585a16f85ba1f5a00017654071f8e3a457a
Marek Szuba <mszuba01@gmail.com>
2020-08-29 15:58:10 +0200

    Add basic animated background elements

---
+++ index.js
@@ -0,0 +1,20 @@
+document.addEventListener('DOMContentLoaded', function () {
+  const elements = document.querySelectorAll('.anim-bg');
+
+  document.addEventListener('mousemove', function (e) {
+
+    const { clientX, clientY } = e;
+    const centerX = window.innerWidth / 2;
+    const centerY = window.innerHeight / 2;
+
+    elements.forEach(element => {
+      const ratioX = -element.getAttribute('ratioX');
+      const ratioY = -element.getAttribute('ratioY');
+
+      const moveX = clientX - centerX
+      const moveY = clientY - centerY
+
+      element.style.transform = `translate(${moveX * ratioX}px, ${moveY * ratioY}px)`
+    })
+  })
+})
