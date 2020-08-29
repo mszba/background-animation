72dcf6e643f713270f7f2526dc1c9b22595701bc
Marek Szuba <mszuba01@gmail.com>
2020-08-29 16:16:00 +0200

    Create animbg class and add it

---
+++ Animbg.js
@@ -0,0 +1,24 @@
+class AnimBg {
+  constructor(selector) {
+    this.elements = document.querySelectorAll(selector);
+  }
+
+  listenCursorMove(e) {
+    const { clientX, clientY } = e;
+    const centerX = window.innerWidth / 2;
+    const centerY = window.innerHeight / 2;
+
+    const posX = clientX - centerX;
+    const posY = clientY - centerY;
+
+    this.elements.forEach(element => this.moveElement(element, posX, posY))
+  }
+
+  moveElement(element, posX, posY) {
+    const ratioX = -element.getAttribute('ratioX');
+    const ratioY = -element.getAttribute('ratioY');
+
+
+    element.style.transform = `translate(${posX * ratioX}px, ${posY * ratioY}px)`
+  }
+}
